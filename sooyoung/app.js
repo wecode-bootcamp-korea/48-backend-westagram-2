23d1a29f87142d9d6a09013fa82443f87d139ad7
김수영 <sooyoungkim@SOOYOUNGKIMmacbookAir.local>
2023-08-08 17:07:02 +0900

    [ADD]게시물 등록 엔드포인트

--- sooyoung/app.js
+++ sooyoung/app.js
@@ -1,57 +1,10 @@
-// require("dotenv").config();
-
-// const express = require('express');
-// const cors = require('cors');
-// const morgan = require('morgan');
-// const { DataSource } = require('typeorm');
-
-// const app = express();
-
-// const appDataSource = new DataSource({
-//     type: process.env.DB_CONNECTION,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     username: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE
-// })
-
-
-// appDataSource.initialize()
-//     .then(() => {
-//         console.log("Data Source has been initialized!")
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-// app.use(express.json());
-// app.use(cors());
-// app.use(morgan('dev'));
-    
-// app.get("/ping", (req, res) => {
-//     res.json({ message : "pong"});
-// });
-
-// app.listen(PORT, async () => {
-//     await appDataSource
-//       .initialize()
-//       .then(() => {
-//         console.log('Data Source has been initialized!');
-//       })
-//       .catch((error) => {
-//         console.error('Error during Data Source initialization', error);
-//       });
-  
-//     console.log(`Listening to request on port: ${PORT}`);
-// });
-
 require("dotenv").config();
-// 사용할 library를 미리 불러옴.
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { DataSource } = require("typeorm");
-// DB setting
+
 const myDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
   host: process.env.TYPEORM_HOST,
@@ -63,7 +16,7 @@ const myDataSource = new DataSource({
 myDataSource.initialize().then(() => {
   console.log("Data Source has been initialized!");
 });
-// express server 만들기 시작
+
 const app = express();
 app.use(cors());
 app.use(morgan("combined"));
