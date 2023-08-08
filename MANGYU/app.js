78bfaff21056a05a6ab4ff078f1a4007724bda00
kimmangyu <vlsual0917@gmail.com>
2023-08-08 16:17:07 +0900

    [ADD] 게시물 등록(생성) 엔드포인트

--- MANGYU/app.js
+++ MANGYU/app.js
@@ -1,49 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors    = require('cors');
-const morgan  = require('morgan');
-const { DataSource } = require('typeorm');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { DataSource } = require("typeorm");
+
+const myDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
+
+myDataSource.initialize().then(() => {
+  console.log("Data Source has been initialized!");
+});
 
 const app = express();
 
-const { DataSource } = require('typeorm')
-
-const appDataSource = new DataSource({
-  type: process.env.DB_CONNECTION,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-})
-
-appDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  });
-
-app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("combined"));
 
-app.get("/ping", (req, res) => {
-    res.json({ message : "pong"});
+app.get("/ping", function (req, res, next) {
+  res.json({ message: "pong" });
 });
 
-app.listen(PORT, async () => {
-  await appDataSource
-    .initialize()
-    .then(() => {
-      console.log('Data Source has been initialized!');
-    })
-    .catch((error) => {
-      console.error('Error during Data Source initialization', error);
-    });
-
-  console.log(`Listening to request on port: ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
-
 
 
 
