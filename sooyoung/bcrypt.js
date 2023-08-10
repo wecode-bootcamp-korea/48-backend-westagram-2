df3be3e4684161d21e83b7ede04014efa340226f
김수영 <sooyoungkim@SOOYOUNGKIMmacbookAir.local>
2023-08-11 00:13:51 +0900

    [ADD]게시물 등록 엔드포인트

--- sooyoung/bcrypt.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcrypt");
-const password = 'password';
-const saltRounds = 12;
-const makeHash = async (password, saltRounds) => {
-    return await bcrypt.hash(password, saltRounds)
-}
